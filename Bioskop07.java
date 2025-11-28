dd308d158489861851a86b8cdf68c05e52eaf01c
auliazahwaa007 <auliazahwaa007@gmail.com>
2025-11-28 14:52:15 +0700

    modifikasi 4

--- Bioskop07.java
+++ Bioskop07.java
@@ -24,8 +24,8 @@ public class Bioskop07 {
         
         System.out.println("Penonton pada baris ke-3: ");
 
-        for (int i = 0; i < penonton[2].length; i++){
-            System.out.println(penonton[2][i]);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
         }
     }
 }
